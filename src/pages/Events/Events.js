aa8492c3c853e4d4d5f0d123f9fe89d8c8e66023
Nazhim <nazhimkalam@gmail.com>
2021-03-11 13:28:32 +0530

    added facebook link

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -6,7 +6,8 @@ import styled from 'styled-components';
 function Events() {
 	const LOADING_GIF_URL = 'https://i.stack.imgur.com/UUjhE.gif';
 	const [loading, setLoading] = useState(true);
-	const STREAMING_URL = 'https://www.youtube.com/watch?v=ug50zmP9I7s';
+	const STREAMING_URL =
+		'https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRogstream%2Fvideos%2F726106508084877%2F&show_text=false&width=560';
 
 	useEffect(() => {
 		setTimeout(() => {
@@ -22,16 +23,25 @@ function Events() {
 					{loading ? (
 						<img src={LOADING_GIF_URL} alt="" />
 					) : (
-						<ReactPlayer
-							loop={true}
-							muted={false}
-							controls={true}
-							playing={true}
-							volume={10}
-							width="100%"
-							height="100%"
-							url={STREAMING_URL}
-						/>
+						// <ReactPlayer
+						// 	loop={true}
+						// 	muted={false}
+						// 	controls={true}
+						// 	playing={true}
+						// 	volume={10}
+						// 	width="100%"
+						// 	height="100%"
+						// 	url={STREAMING_URL}
+						// />
+						<iframe
+							title="title"
+							src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRogstream%2Fvideos%2F726106508084877%2F&show_text=false"
+							className="facebook__live"
+							frameborder="0"
+							gesture="media"
+							allow="autoplay; encrypted-media"
+							allowfullscreen
+						></iframe>
 					)}
 				</div>
 			</Fade>
@@ -46,6 +56,18 @@ const EventsMainContainer = styled.div`
 	animation: fadeInAnimation ease 1s;
 	margin: 100px 0;
 
+	iframe {
+		overflow: hidden;
+	}
+
+	.facebook__live {
+		border: none !important;
+		object-fit: contain !important;
+
+		width: 100% !important;
+		height: 700px !important;
+	}
+
 	.eventsTitle {
 		font-size: 2rem;
 		margin-bottom: 30px;
@@ -58,19 +80,33 @@ const EventsMainContainer = styled.div`
 	}
 	.streamingVideo {
 		margin: 30px 50px;
-		background-color: #eee;
-		border: 1px lightgreen solid;
+		/* background-color: #eee; */
+		/* border: 1px lightgreen solid; */
 		display: flex;
-		object-fit: contain;
-		height: 500px;
+		/* height: 600px; */
 		align-items: center;
 		justify-content: center;
+
+		height: 100% !important;
 	}
 	@media screen and (max-width: 950px) {
 		margin: 80px 0 120px 0;
+
+		.facebook__live {
+			height: 500px !important;
+		}
+	}
+	@media screen and (max-width: 700px) {
+		.facebook__live {
+			height: 400px !important;
+		}
 	}
 	@media screen and (max-width: 500px) {
 		margin: 50px 0 80px 0;
+
+		.facebook__live {
+			height: 300px !important;
+		}
 
 		.streamingVideo {
 			margin: 30px;
@@ -80,6 +116,9 @@ const EventsMainContainer = styled.div`
 	@media screen and (max-width: 400px) {
 		.streamingVideo {
 			margin: 10px;
+		}
+		.facebook__live {
+			height: 250px !important;
 		}
 	}
 `;
